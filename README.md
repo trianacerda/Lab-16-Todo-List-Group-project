4af78feecaa0ff9ece1f0bbc3615303f8273b7c3
Triana Cerda <trianaRcerda@gmail.com>
2021-07-22 15:40:44 -0700

    added searchUser function

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Lab-16-Todo-List-Group-project
 to do list for project 1 
 ![Lab-16-workflow](https://user-images.githubusercontent.com/82128225/126684046-823492e7-b954-478b-ae69-a09a7ae9c25f.jpg)
-https://files.slack.com/files-pri/T6FCZF1HR-F028H5WP58X/img_1933.jpg
--- local-storage/local-storage.js
+++ local-storage/local-storage.js
@@ -1,19 +1,47 @@
 export const USERS = 'users';
 
 export function getUser() {
-    const DATA = localStorage.getItem(USER);
-    const arrUsers = JSON.parse(arrUsers);
+    const DATA = localStorage.getItem(USERS || '[]');
+    const arrUsers = JSON.parse(DATA);
     return arrUsers;
 }
 
 
 
-export function setUser(obj){
+// export function setUser(obj){
+//     const userString = JSON.stringify(obj);
+//     localStorage.setItem(USERS, userString);
+// }
 
-    const userString = JSON.stringify(obj);
+//create search user
+//function input is a user
+//getUser();
+//loop through LS -- looking for user name/text entered matches object already there
+//IF true-- return user object from LS
 
-    localStorage.push(USER, userString);
+export function searchUser(userName){
+    const users = getUser();
+    for (let user of users){
+        if (user.username === userName){
+        return user;}
+    }
+
 }
+
+
+
+
+//create a new user
+//getUser(); -- getting it from LS
+//need to check if the user already exists 
+//if user does NOT exist, create a new user
+//need to push new user to LS
+//need to set a NEW LS 
+
+export function createUser(){
+
+}
+
 
 
 //this creates a new user 
